1223065dd4542176319fb5456eabe58c6760da8f
samped <michaelsamuelpedro@gmail.com>
2023-03-22 14:39:14 +0100

    setup

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator")
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "please enter an email"] ,
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, "please enter a valid email"]
+    },
+    password: {
+        type: String,
+        required: [true, "please enter a password"],
+        minlength: [8, "Minimum password length is 8 characters"]
+    },
+})
+
+
+// fire a function after doc saved to db
+
+userSchema.post("save", function (doc, next){
+    console.log("new user was created & saved", doc)
+
+    next();
+});
+
+
+// fire a function before doc saved to db
+userSchema.pre("save", async function (next){
+    const salt = await bcrypt.genSalt();
+   this.password = await bcrypt.hash(this.password, salt);
+    next();
+
+})
+
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
